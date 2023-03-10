b12feecdb393a55d219dab248d549a7e544170d9
Ishani Mukund Naik <ishaninaik598@gmail.com>
2023-03-10 14:12:34 +0530

    Added Async and await code

--- AsyncAndAwait/Program.cs
+++ AsyncAndAwait/Program.cs
@@ -11,6 +11,7 @@ namespace AsyncAndAwait
     {
         static void Main(string[] args)
         {
+
             //This task will as the which task having less time.
             Task1();
             Task2();
